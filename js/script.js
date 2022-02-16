503c45366846f36accd1a89bb5404794bbf45817
Cube-Box-391 <adrian.duqueluengo@gmail.com>
2022-02-16 17:58:20 +0100

    16/2/2022

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let nombre="adrian"
 let telefono="96849816"
 let apellido="duque"
 
-alert("hola mundo")
+//alert("hola mundo")
 
 console.log(producto)
 console.log(disponible)
@@ -19,6 +19,21 @@ console.log(producto2)
 let producto3 = new String("monitor de 30 pulgadas")
 console.log(producto3)
 
+let tv="pantalla 25\""
+
+console.log(tv)
 console.log(typeof(apellido))
 console.log(typeof(producto2))
 console.log(typeof(producto3))
+
+let cp="28691"
+longitud_cp=cp.length
+console.log(cp)
+console.log(longitud_cp)
+
+let txt="hola hola"
+console.log(txt)
+let txt_c=txt
+txt=txt.toUpperCase
+console.log(txt.indexOf('hola'))
+console.log(txt_c)
