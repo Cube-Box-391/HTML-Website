97001c5796e15b398f269305079d9025af4b6f6b
Cube-Box-391 <adrian.duqueluengo@gmail.com>
2022-03-02 17:56:26 +0100

    2/3/22

--- js/js1.js
+++ js/js1.js
@@ -40,3 +40,90 @@ console.log(puntos)
 
 const bool = true //solo para true o false
 console.log(bool)
+
+//objects
+
+const producto20 = {
+    nombre:'Monitor',
+    precio:300,
+    disponible:true
+}
+
+console.log(producto20)
+console.log(producto20.precio)
+console.log(producto20["nombre"])
+
+producto20.marca = 'HP'
+
+console.log(producto20)
+
+// delete producto20.precio
+
+// console.log(producto20)
+
+producto20.precio= 20
+
+const valorPrecio = producto20.precio
+
+console.log(valorPrecio)
+
+const {precio} = producto20
+console.log(precio)
+
+Object.freeze(producto20) //disables object edditing
+
+producto20.precio = 50
+
+console.log(producto20)
+
+const alumno = {
+    nombre:'Adrian',
+    apellido:'Duque',
+    curso:'3C'
+}
+
+console.log(alumno)
+console.log(alumno.apellido)
+
+Object.freeze(alumno)
+
+alumno.nombre = 'juan'
+console.log(alumno)
+
+const medidas = {
+    peso: '1kg',
+    altura:'1m'
+}
+
+console.log(medidas)
+
+const lasMedidas = {...alumno, ...medidas}
+console.log(lasMedidas)
+
+const coche = {
+    marca:'ferrari',
+    modelo:'458 italia',
+    motor:'500 caballos',
+    color:'rojo'
+}
+
+console.log(coche)
+
+const num = [10,20,30,40,50]
+
+console.log(num)
+
+console.table(num)
+
+const meses = new Array ('enero', 'febrero', 'marzo')
+
+console.log(meses)
+console.log(meses[1])
+
+const nombres = ['antonio', 'juan', 'maria']
+
+console.table(nombres)
+
+const array = ['string', 100, true, null, [1,2,3], moto={marca:'vespa', motor:'125CC'}]
+
+console.log(array)
